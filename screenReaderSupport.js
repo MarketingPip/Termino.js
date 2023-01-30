03b27a6cf3e1b3f8abf9153e636f8843b180e704
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2023-01-30 10:15:56 -0500

    Update screenReaderSupport.js

--- screenReaderSupport.js
+++ screenReaderSupport.js
@@ -55,3 +55,29 @@ function screenReaderSupport() {
 }
 /* example usage screenReaderSupport() */
 screenReaderSupport();
+
+
+
+
+
+
+
+
+
+
+```
+const allVoicesObtained = new Promise(function(resolve, reject) {
+  let voices = window.speechSynthesis.getVoices();
+  if (voices.length !== 0) {
+    resolve(voices);
+  } else {
+    window.speechSynthesis.addEventListener("voiceschanged", function() {
+      voices = window.speechSynthesis.getVoices();
+      resolve(voices);
+    });
+  }
+});
+
+allVoicesObtained.then(voices => console.log("All voices:", voices));
+
+```
