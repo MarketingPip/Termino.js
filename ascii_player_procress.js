63c13bc540d678239dfc216ee5f61c26d567d9ea
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2023-02-26 15:55:34 -0500

    Update ascii_player_procress.js

--- ascii_player_procress.js
+++ ascii_player_procress.js
@@ -10,3 +10,32 @@ function playAnimation(animation) {
     }
   }, 100);
 }
+
+
+/* function to output to process based using stout. node js process app */
+function output(text) {
+  process.stdout.write(text);
+}
+/* similar function but to stdout value and remove it after. */
+function outputAndRemove(text) {
+  var div = document.createElement('div');
+  div.innerHTML = text;
+  document.body.appendChild(div);
+  setTimeout(function() {
+    document.body.removeChild(div);
+  }, 1000);
+}
+/* similar function but for node js procress app */
+function outputAndRemoveNode(text) {
+  process.stdout.write(text);
+  setTimeout(function() {
+    process.stdout.write('\b \b');
+  }, 1000);
+}
+/* similar function for node.js based app but remove string from procress from provided variable. */
+function outputAndRemoveNode(text) {
+  process.stdout.write(text);
+  setTimeout(function() {
+    process.stdout.write('\b \b');
+  }, 1000);
+}
